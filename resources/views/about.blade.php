51f5f514bb64112a8039711f22b0f5a13639dcc2
beckrecca <rebecca.doris42@gmail.com>
2015-05-21 22:02:40 -0400

    First commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,47 @@
+@extends('master')
+
+@section('title', 'Aspiring Computer Person')
+
+@section('header')
+    <header>
+    	<div class="jumbotron">
+    		<div class="container">
+	    		<h1>Hello!</h1>
+	    		<p>My name is Rebecca. I love to create things, especially using web technologies.</p>
+	    	</div>
+	    </div>
+	</header>
+@stop
+
+@section('content')
+	<h4>Right Now</h4>
+
+	<p>
+		Currently, I am pursuing a Master of Liberal Arts (ALM) at Harvard Extension School.
+		I am concentrating in Information Technology with a major in Digital Media Arts and
+		Instructional Design. I am passionate about my studies, and I dream of working
+		in tech someday. 
+	</p>
+
+	<h4>How did I get here?</h4>
+	<p>
+		In the late eighties, I was a baby who didn't know anything. I had a lot to learn, 
+		which was really fortunate for me, since learning is definitely my favorite thing. 
+	</p>
+	<p>
+		By the late aughts, I was a much taller baby who had learned a lot of things. I 
+		had ecstatically geeked out at Boston University, where I developed strong analytical 
+		skills studying the most productive, recursive and powerful tool humanity has: language! 
+		I knew how to read speech sounds from a spectrogram, diagram complex grammar into syntax 
+		trees, distinguish an allophone from a phoneme, and identify which of Grice's maxims an 
+		awkward person had just violated, among other things. I understood that the best way to 
+		learn a language was to immerse yourself in it after a semester abroad.
+	</p>
+	<p>
+		My affinity for creative systems and love of learning drew me to web technologies. 
+		In the tech world, there's always something new to learn. 
+		(Heck, I just had to re-learn Laravel's newest version to build this site!)
+		I found that a linguistics B.A. 
+		had provided a solid foundation in problem-solving that translated well to programming. 
+	</p>
+@stop
