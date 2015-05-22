b9f57081537340bfa71e5328d95c9533830a906d
beckrecca <rebecca.doris42@gmail.com>
2015-05-21 23:10:30 -0400

    more resume and about me

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,6 @@
 @extends('master')
 
-@section('title', 'Aspiring Computer Person')
+@section('title', 'About Rebecca')
 
 @section('header')
     <header>
--- resources/views/resume.blade.php
+++ resources/views/resume.blade.php
@@ -1,6 +1,6 @@
 @extends('master')
 
-@section('title', 'Aspiring Computer Person')
+@section('title', 'Resume')
 
 @section('header')
     <header>
@@ -17,30 +17,34 @@
 
 	<p>
 		<span class="resume-date">January 2011 – present</span> <br>
-		Harvard Extension School, Cambridge, MA <br>
+		<a href="http://extension.harvard.edu" target="_blank">Harvard Extension School</a>, Cambridge, MA <br>
 		Candidate for ALM in Extension Studies, admitted January 2014 <br>
 		Concentration: Information Technology <br>
-		Current GPA: 3.93  <br>
-		Major: Digital Media Arts and Instructional Design
+		Major: Digital Media Arts and Instructional Design <br>
+		Current GPA: 3.93 
 	</p>
 
 	<p>
 		<span class="resume-date">September 2006 – January 2010</span>  <br>
-		Boston University, Boston, MA  <br>
+		<a href="http://bu.edu" target="_blank">Boston University</a>, Boston, MA  <br>
 		Major: Linguistics 	 <br>
 		Minor: French 	 <br>	
 		GPA: 3.82  <br>
-		Awards: Barbara Argote Junior Award in Linguistics 2009, Ken Hale Award 2010 <br>
-		   College of Arts and Sciences College Scholars Member
+		Awards: 
+		<ul class="resume-list">
+			<li><a href="http://ling.bu.edu/people/awards" target="_blank">Barbara Argote Junior Award in Linguistics 2009</a></li>
+			<li><a href="http://ling.bu.edu/people/awards">Ken Hale Award 2010</a></li>
+		   	<li>College of Arts and Sciences College Scholars Member</li>
+		</ul>
 	</p>
 
 	<h3>Work Experience</h3>
 
 	<p>
 		<span class="resume-date">October 2010 – present</span><br>
-		Harvard Extension School, Academic Services Office, Cambridge MA<br>
+		<a href="http://extension.harvard.edu" target="_blank">Harvard Extension School</a>, Academic Services Office, Cambridge MA<br>
 		Records Specialist
-		<ul>
+		<ul class="resume-list">
 			<li>In-person, telephone, and email reception and for a high-volume student service office.</li>
 			<li>Process requests for transcripts and letters of enrollment, research student records, verify student status, maintain confidentiality policies.</li>
 		</ul>
@@ -48,9 +52,9 @@
 
 	<p>
 		<span class="resume-date">February 2010 – August 2010</span><br>
-		Harvard Summer School, Registrar’s Office, Cambridge MA <br>
+		<a href="http://summer.harvard.edu" target="_blank">Harvard Summer School</a>, Registrar’s Office, Cambridge MA <br>
 		I-20 Assistant 
-		<ul>
+		<ul class="resume-list">
 			<li>Process paper applications for the I-20 Certificate of Eligibility for a student visa. </li>
 			<li>Timely, accurate data entry, handling a high volume of submissions.</li>
 		</ul>
@@ -58,9 +62,9 @@
 
 	<p>
 		<span class="resume-date">May 2008 – January 2010 (during summer and winter breaks)</span> <br>
-		The Community Bank (now Eastern Bank), Bridgewater MA <br>
+		<a href="http://www.communitybank.com" target="_blank">The Community Bank</a> (now Eastern Bank), Bridgewater MA <br>
 		Teller 
-		<ul>
+		<ul class="resume-list">
 			<li>Provide customer service, answer phones, sell products, contact customers with updates. </li>
 			<li>Handle large cash transactions, balance ATM transactions, scan deposited checks, file reports. </li>
 		</ul>
@@ -68,9 +72,9 @@
 
 	<p>
 		<span class="resume-date">September 2006 – December 2009 (during Fall and Spring semesters)</span> <br>
-		College of Arts and Sciences Business Office, Boston University, Boston MA <br>
+		<a href="http://www.bu.edu/cas/" target="_blank">College of Arts and Sciences</a> Business Office, Boston University, Boston MA <br>
 		Office Assistant 
-		<ul>
+		<ul class="resume-list">
 			<li>Student employee assisting reception, accounting and payroll staff.</li>
 			<li>Answer phones, file records, copy and hand-deliver documents, make bank deposits, pick up and distribute paychecks.</li>
 		</ul>
